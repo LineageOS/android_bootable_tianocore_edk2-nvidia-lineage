f9ba9e4a0231d47de9391fc756aeee90a5616cb7
Aaron Kling <webgeek1234@gmail.com>
2023-10-07 15:14:33 -0500

    AndroidLauncher: Don't use scratch register
    Google kernel requirements state that devmem access is not allowed. Thus
    convert retry count to an efi var like status.
    
    Change-Id: I9113a5682943759206858973517ec7a5a0d4cbba

--- Silicon/NVIDIA/Application/AndroidLauncher/L4TRootfsValidation.h
+++ Silicon/NVIDIA/Application/AndroidLauncher/L4TRootfsValidation.h
@@ -10,45 +10,14 @@
 #ifndef __ROOTFSVALIDATIONPRIVATE_H__
 #define __ROOTFSVALIDATIONPRIVATE_H__
 
-/*
- * Rootfs Scratch register
- *
- * 00:15 magic 'FACE'
- * 16:17 Current rootfs slot
- * 18:19 Retry count of rootfs slot B
- * 20:21 Retry count of rootfs slot A
- * 22:31 reserved
- */
-#define SR_RF_MAGIC_MASK  0x0000FFFFU
-#define SR_RF_MAGIC       0xFACEU   /* 'FACE' */
-
-#define RF_CURRENT_SLOT_SHIFT   16U
-#define RF_CURRENT_SLOT_MASK    (0x03U << RF_CURRENT_SLOT_SHIFT)
-#define RF_RETRY_COUNT_B_SHIFT  18U
-#define RF_RETRY_COUNT_B_MASK   (0x03U << RF_RETRY_COUNT_B_SHIFT)
-#define RF_RETRY_COUNT_A_SHIFT  20U
-#define RF_RETRY_COUNT_A_MASK   (0x03U << RF_RETRY_COUNT_A_SHIFT)
-
-#define SR_RF_MAGIC_GET(reg)  ((reg) & SR_RF_MAGIC_MASK)
-#define SR_RF_MAGIC_SET(reg)  (((reg) & ~SR_RF_MAGIC_MASK) | SR_RF_MAGIC)
-
-#define SR_RF_CURRENT_SLOT_GET(reg)        (((reg) & RF_CURRENT_SLOT_MASK) >> RF_CURRENT_SLOT_SHIFT)
-#define SR_RF_CURRENT_SLOT_SET(slot, reg)  (((reg) & ~RF_CURRENT_SLOT_MASK) |   \
-                                             (((slot) & 0x03U) << RF_CURRENT_SLOT_SHIFT))
-
-#define SR_RF_RETRY_COUNT_B_GET(reg)         (((reg) & RF_RETRY_COUNT_B_MASK) >> RF_RETRY_COUNT_B_SHIFT)
-#define SR_RF_RETRY_COUNT_B_SET(count, reg)  (((reg) & ~RF_RETRY_COUNT_B_MASK) | \
-                                             (((count) & 0x03U) << RF_RETRY_COUNT_B_SHIFT))
-
-#define SR_RF_RETRY_COUNT_A_GET(reg)         (((reg) & RF_RETRY_COUNT_A_MASK) >> RF_RETRY_COUNT_A_SHIFT)
-#define SR_RF_RETRY_COUNT_A_SET(count, reg)  (((reg) & ~RF_RETRY_COUNT_A_MASK) | \
-                                             (((count) & 0x03U) << RF_RETRY_COUNT_A_SHIFT))
-
 #define ROOTFS_SLOT_A  0
 #define ROOTFS_SLOT_B  1
 
-#define FROM_REG_TO_VAR  0
-#define FROM_VAR_TO_REG  1
+#define ROOTFS_RETRY_MAX 7
+
+#define ANDROIDLAUNCHER_STATUS_NORMAL     NVIDIA_OS_STATUS_NORMAL
+#define ANDROIDLAUNCHER_STATUS_UNBOOTABLE NVIDIA_OS_STATUS_UNBOOTABLE
+#define ANDROIDLAUNCHER_STATUS_BOOTING    0x01
 
 #define DELAY_SECOND  1000000
 
@@ -56,8 +25,8 @@ typedef enum {
   RF_STATUS_A,
   RF_STATUS_B,
 
-  RF_REDUNDANCY,
-  RF_RETRY_MAX,
+  RF_RETRY_A,
+  RF_RETRY_B,
 
   RF_FW_NEXT,
   RF_BC_STATUS,
@@ -72,8 +41,6 @@ typedef struct {
 
 typedef struct {
   RF_VARIABLE    RootfsVar[RF_VARIABLE_INDEX_MAX];
-  UINT32         RetryCountSlotA;
-  UINT32         RetryCountSlotB;
   UINT32         CurrentSlot;
 } L4T_RF_AB_PARAM;
 
